277c579b1295727316e18242d67a93b3a805d958
Sara Gdbg <146811766+SaraGdbg@users.noreply.github.com>
2024-08-15 14:47:47 +0200

    🎨 Add cmonents

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-
-import './App.css'
+import "./App.css";
+import { Countdown } from "./components/Countdown";
+import { Generator } from "./components/Generator";
 
 function App() {
-  
-
   return (
     <>
-
+      <Countdown></Countdown>
+      <Generator></Generator>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
