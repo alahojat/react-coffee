017af1ba8eee3bc02df9f951a8a537aba39b0097
Ala Hojat Jalali <alahojatjalali@gmail.com>
2024-08-15 16:25:04 +0200

    ✨ image generator

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -1,3 +1,42 @@
+import  { useState } from 'react';
+import coffee from '../assets/images/coffee.jpg';
+import coffeeZen from '../assets/images/coffeeZen.jpg';
+import dog from '../assets/images/dog.jpg';
+import emotionalSupport from '../assets/images/emotionalSupport.jpg';
+import minion from '../assets/images/minion.jpg';
+import noPunching from '../assets/images/noPunching.jpg';
+import sanity from '../assets/images/sanity.jpg';
+import squirrel from '../assets/images/squirrel.jpg';
+import unicorn from '../assets/images/unicorn.jpg';
+import youProblem from '../assets/images/youProblem.jpg';
+
 export const Generator = () => {
-  return <span>Generator här</span>;
+  const images: { image: string }[] = [
+    { image: coffee },
+    { image: coffeeZen },
+    { image: dog },
+    { image: emotionalSupport },
+    { image: minion },
+    { image: noPunching },
+    { image: sanity },
+    { image: squirrel },
+    { image: unicorn },
+    { image: youProblem },
+  ];
+
+  const [imageSource, setImageSource] = useState('');
+
+  const getRandomImage = () => {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    setImageSource(images[randomIndex].image);
+  };
+
+  return (
+    <>
+      <button onClick={getRandomImage}>Make me happy</button>
+      <div>
+        {imageSource && <img src={imageSource} />}
+      </div>
+    </>
+  );
 };
